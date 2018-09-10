890e9218b7fc4df8cbab4f3f0d7d01b649698549
y_xingyu@126.com <y_xingyu@126.com>
2018-09-10 17:31:30 +0800

    first commit

---
+++ app/src/main/java/com/rd/utils/ed/activity/Ed_Activity_Activity.java
@@ -0,0 +1,61 @@
+package com.rd.utils.ed.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.rd.utils.R;
+
+/**
+ * Android 组件一  Activity
+ * 本业包含
+ *      1.Activity的生命周期
+ *      2.
+ *
+ */
+public class Ed_Activity_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ed_activity);
+
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/rd/utils/ui/MainActivity.java
@@ -0,0 +1,51 @@
+package com.rd.utils.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.rd.utils.R;
+import com.rd.utils.ed.activity.Ed_Activity_Activity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id==R.id.ed_btn_main){
+            Intent intented = new Intent();
+            intented.setClass(this, Ed_Activity_Activity.class);
+            startActivity(intented);
+        }
+        else if(id==R.id.tool_btn_main){
+
+        }
+        else if(id == R.id.other_btn_main){
+
+        }
+        else if(id==R.id.third_btn_main){
+
+        }
+    }
+}
